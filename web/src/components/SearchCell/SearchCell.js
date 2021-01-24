8a4bda23b0d5a2b42800c7aa2a02f38232e6c950
las1m0n <bondarenkonikita295@gmail.com>
2021-01-24 13:27:08 +0200

    add search cell

--- web/src/components/SearchCell/SearchCell.js
+++ web/src/components/SearchCell/SearchCell.js
@@ -1,17 +1,21 @@
-import {Form, Label, TextAreaField, Submit, useQuery} from "@redwoodjs/web";
-import ItemsCell from "src/components/ItemsCell/ItemsCell";
-import {useEffect, useState} from "react";
-import axios from 'axios';
-import React from "react";
+import Item from "src/components/Item/Item";
 
-const QUERY = gql`
+export const QUERY = gql`
   query FIND_ITEM_BY_ID($name: String!) {
-    item(name: $name) {
+    findItem(name: $name) {
       id
       name
-      shop
-      url
-      currentPrice
+      about
+      image
+      priceInfo {
+        id
+        url
+        shop {
+          id
+          name
+          imageUrl
+        }
+      }
       priceMap {
         id
         price
@@ -21,61 +25,12 @@ const QUERY = gql`
   }
 `
 
-const getInfo = () => {
-  axios.get(url)
-  return response.data?.results[0]
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({error}) => <div>Error: {error.message}</div>
+
+export const Success = ({findItem}) => {
+  return findItem.map((item) => <Item key={item.id} item={item} />)
 }
-
-const SearchCell = (props) => {
-  const [formState, setFormState] = useState({
-    name: "",
-    results: []
-  });
-
-  const {loading, error, data} = useQuery(QUERY, {
-    variables: {name: formState.name}
-  })
-
-  useEffect(() => {
-    // Оновлюємо заголовок документа, використовуючи API браузера
-
-  }, []);
-
-
-  return (
-    <div className="form-group">
-      <input
-        value={formState.name}
-        onChange={(e) =>
-          setFormState({
-            ...formState,
-            name: e.target.value
-          })
-        }
-        type="text"
-        className="form-control"
-        placeholder="Поиск"
-      />
-
-      <div>
-        {data?.item?.map((it, index) => (
-          <ul key={index}>
-            <li>
-              <p>Магазин: {it.shop} Товар: {it.name}</p>
-              График
-              {it.priceMap.map((i, ind) => (
-                <div key={ind}>
-                  <p>{i.price}</p>
-                  <p>{i.date}</p>
-                </div>
-              ))}
-            </li>
-            <hr/>
-          </ul>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-export default SearchCell
